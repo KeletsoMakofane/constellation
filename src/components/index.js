e72c3d8c7c2c934c9d34edd4f11a0e9bbfae88cb
KeletsoMakofane <65560061+KeletsoMakofane@users.noreply.github.com>
2021-10-23 21:40:05 -0400

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { NeoGraph } from './neograph';
-export { ResponsiveNeoGraph } from './neograph';
-export { default as NeoGraphContainer } from './neographcontainer';
-export { default as Header } from './header';
-export { default as Footer } from './footer';
-export { default as updateName} from './autocomplete';
-export { default as credentials} from './credentials';
