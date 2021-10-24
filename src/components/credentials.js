73550bdfe50c113e61e3e74e8dfd5c2315ba8626
KeletsoMakofane <65560061+KeletsoMakofane@users.noreply.github.com>
2021-10-23 21:12:15 -0400

    check security

--- src/components/credentials.js
+++
@@ -1,8 +0,0 @@
-const credentials = {
-    NEO4J_URI:  "neo4j://graph.keletsomakofane.com",
-    NEO4J_USER: "neo4j",
-    NEO4J_PASSWORD: "waiter-vega-blue-cave-shrink-1769"};
-
-///
-
-export default credentials;
